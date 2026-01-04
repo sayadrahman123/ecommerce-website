29d166a65d35494262b25b092f0090215df8b874
abdus <abdusrahman64@gmail.com>
2026-01-04 19:26:35 +0530

    Disable Seeder and fix Dialect

--- backend/src/main/java/com/rahmani/backend/config/DataSeeder.java
+++ backend/src/main/java/com/rahmani/backend/config/DataSeeder.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-@Component
+//@Component
 public class DataSeeder implements CommandLineRunner {
 
     private final ProductRepository productRepository;
